c305eac5f0b26ca29f78b9217c7bfe9252f136c6
Joshua Oloton <olotonjoshua@gmail.com>
2024-06-26 17:43:19 +0100

    configured production wsgi server

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
 from flask import Flask
 from api import create_app
+from waitress import serve
+from paste.translogger import TransLogger
+import logging
+logger = logging.getLogger('waitress')
+logger.setLevel(logging.INFO)
 
 
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True, port=3000)
+    # app.run(debug=True, port=3000)
+    # serve(app, host='127.0.0.1', port=8080)
+    serve(TransLogger(app, setup_console_handler=False))
