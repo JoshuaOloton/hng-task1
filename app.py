8b38d3310f9f587d188c46e083a858f99ecfdf48
Joshua Oloton <olotonjoshua@gmail.com>
2024-06-26 22:57:41 +0100

    configured wsgi port number

--- app.py
+++ app.py
@@ -12,4 +12,4 @@ app = create_app()
 if __name__ == '__main__':
     # app.run(debug=True, port=3000)
     # serve(app, host='127.0.0.1', port=8080)
-    serve(TransLogger(app, setup_console_handler=False))
+    serve(TransLogger(app, setup_console_handler=False), host='127.0.0.1')
